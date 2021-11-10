4c4a1a97ffa042dfe07ad56a05e0d2ec8d79ae51
Marcelo de Carvalho Santana <marcelo.scientist@hotmail.com>
2021-11-10 16:33:46 -0300

    Iniciando a criação do Model Cliente

--- src/main/java/br/com/bluebank/squad3/models/Cliente.java
+++ src/main/java/br/com/bluebank/squad3/models/Cliente.java
@@ -1,5 +1,101 @@
 package br.com.bluebank.squad3.models;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.validation.constraints.Email;
+import java.io.Serializable;
 
-public class Cliente {
+@Entity
+@Table(name = "clientes")
+public class Cliente implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id_cliente;
+
+    @NotNull
+    @Size(max = 100)
+    @Column(name = "nome")
+    private String nome;
+
+    @NotNull
+    @Size(max = 100)
+    @Column(name = "cpf")
+    private String cpf;
+
+    @NotNull
+    @Size(max = 100)
+    @Column(name = "telefone")
+    private String telefone;
+
+
+    @NotNull
+    @Email
+    @Size(max = 100)
+    @Column(unique = true)
+    private String email;
+
+
+    @OneToOne(fetch = FetchType.LAZY,
+            cascade =  CascadeType.ALL,
+            mappedBy = "cliente")
+    private Endereco endereco;
+
+    public Cliente(Long id_cliente, String nome, String cpf, String telefone, String email, Endereco endereco) {
+        this.id_cliente = id_cliente;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.telefone = telefone;
+        this.email = email;
+        this.endereco = endereco;
+    }
+
+    public Cliente(String nome, String cpf, String telefone, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Endereco getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(Endereco endereco) {
+        this.endereco = endereco;
+    }
+
+    // Getters and Setters (Omitted for brevity)
 }
