aaac3723d820310c51fbf87d3412c5f77aefba20
Marcelo de Carvalho Santana <marcelo.scientist@hotmail.com>
2021-11-22 05:02:34 -0300

    Implementando ClienteService

--- src/main/java/br/com/bluebank/squad3/controllers/ClienteController.java
+++ src/main/java/br/com/bluebank/squad3/controllers/ClienteController.java
@@ -2,11 +2,12 @@ package br.com.bluebank.squad3.controllers;
 
 import java.net.URISyntaxException;
 import java.util.List;
-import java.util.Optional;
 
 import javax.validation.Valid;
 
 //import org.springframework.beans.factory.annotation.Autowired;
+import br.com.bluebank.squad3.services.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -20,55 +21,37 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.bluebank.squad3.models.Cliente;
-import br.com.bluebank.squad3.repositories.ClienteRepository;
 
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
-	//isso é um teste
 
-//	@Autowired
-	private ClienteRepository clienteRepository;
+	private ClienteService clienteService;
 
-	public ClienteController(ClienteRepository clienteRepository) {
-		this.clienteRepository = clienteRepository;
+	@Autowired
+	public
+	void ClienteService(ClienteService clienteService) {
+		this.clienteService = clienteService;
 	}
 
 	@GetMapping("/listar")
 	public ResponseEntity<List<Cliente>> listarClientes() {
-		return ResponseEntity.ok(clienteRepository.findAll());
+		return this.clienteService.listarClientes();
 	}
 	//metodo de listar por id
 	@PostMapping("/cadastrar")
 	@ResponseStatus(HttpStatus.CREATED)
 	public ResponseEntity<Cliente> cadastrarCliente(@RequestBody Cliente cliente) {
-		return ResponseEntity.status(HttpStatus.CREATED).body(clienteRepository.save(cliente));
+		return this.clienteService.cadastrarCliente(cliente);
 	}
 
-//	@PutMapping("/atualizar/{id_cliente}")
-//	public Optional<ResponseEntity<Cliente>> atualizarNome(@PathVariable(value = "id_cliente") Long id_cliente,
-//			@RequestBody Cliente nomeAtualizado) {
-//		return clienteRepository.findById(id_cliente).map(nomeExistente -> {
-//			nomeExistente.setNome(nomeAtualizado.getNome());
-//			return Optional
-//					.ofNullable(ResponseEntity.status(HttpStatus.OK).body(clienteRepository.save(nomeExistente)));
-//
-//		}).orElseGet(() -> {
-//			return Optional.empty();
-//		});
-//	}
-	
 	@PutMapping("/atualizar/{id_cliente}")
 	public ResponseEntity<Cliente> atualizarCliente(@PathVariable Long id_cliente, @RequestBody @Valid Cliente cliente) throws URISyntaxException {
-        clienteRepository.findById(id_cliente);
-        cliente.setId_cliente(id_cliente);
-        clienteRepository.save(cliente);
-    	return new ResponseEntity<>(cliente, HttpStatus.CREATED);
+        return this.clienteService.atualizarCliente(id_cliente, cliente);
     }
 	
 	@DeleteMapping("/deletar/{id_cliente}")
 	public void deletarCliente(@PathVariable Long id_cliente) {
-		clienteRepository.deleteById(id_cliente);
+		this.clienteService.deletarCliente((id_cliente));
 	}
-
 }
