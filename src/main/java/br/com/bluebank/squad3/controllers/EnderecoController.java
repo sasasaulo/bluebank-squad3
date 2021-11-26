835d8cf4c28960c20b777c062922337d56831f3d
Edmir Cabral <edi_csouza@hotmail.com>
2021-11-25 21:14:36 -0300

    ajustando as classes de endereco

--- src/main/java/br/com/bluebank/squad3/controllers/EnderecoController.java
+++ src/main/java/br/com/bluebank/squad3/controllers/EnderecoController.java
@@ -5,12 +5,14 @@ import br.com.bluebank.squad3.services.EnderecoService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.net.URISyntaxException;
 import java.util.List;
 
 @RestController
-@RequestMapping("/endereco")
+@RequestMapping(path = "/endereco")
 public class EnderecoController {
-
 
     private EnderecoService enderecoService;
 
@@ -21,12 +23,19 @@ public class EnderecoController {
     @PostMapping("/cadastrar")
     @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<Endereco> cadastrarEndereco(@RequestBody Endereco endereco) {
-        return this.enderecoService.cadastrarEndereco(endereco);
-
+        return ResponseEntity.status(HttpStatus.CREATED).body(enderecoService.salvar(endereco));
     }
+
     @GetMapping("/listar")
-    public ResponseEntity<List<Endereco>> listarEndereco() {
-        return this.enderecoService.listarEndereco();
+    public ResponseEntity<List<Endereco>> listarEnderecos() {
+
+        return ResponseEntity.ok(enderecoService.listar());
+    }
+
+    @PutMapping("/atualizar/{id_endereco}")
+    public ResponseEntity<Endereco> atualizarEndereco(@PathVariable Long id_endereco, @RequestBody @Valid Endereco endereco) throws URISyntaxException {
+        enderecoService.alterar(id_endereco, endereco);
+        return new ResponseEntity<>(endereco, HttpStatus.CREATED);
     }
 
 }
--- src/main/java/br/com/bluebank/squad3/services/EnderecoService.java
+++ src/main/java/br/com/bluebank/squad3/services/EnderecoService.java
@@ -3,9 +3,6 @@ package br.com.bluebank.squad3.services;
 
 import br.com.bluebank.squad3.models.Endereco;
 import br.com.bluebank.squad3.repositories.EnderecoRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-
 import java.util.List;
 
 public class EnderecoService {
@@ -17,18 +14,18 @@ public class EnderecoService {
         this.enderecoRepository = enderecoRepository;
     }
 
-    public ResponseEntity<Endereco> cadastrarEndereco(Endereco endereco) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(enderecoRepository.save(endereco));
-    }
-    public ResponseEntity<List<Endereco>> listarEndereco() {
-        return ResponseEntity.ok(enderecoRepository.findAll());
+    public Endereco salvar(Endereco endereco) {
+        return enderecoRepository.save(endereco);
     }
 
-    public ResponseEntity<Endereco> atualizarEndereco(Long id_endereco, Endereco endereco) {
+    public List<Endereco> listar() {
+        return enderecoRepository.findAll();
+    }
+
+    public void alterar ( long id_endereco, Endereco endereco){
         enderecoRepository.findById(id_endereco);
         endereco.setId_endereco(id_endereco);
         enderecoRepository.save(endereco);
-        return new ResponseEntity<>(endereco, HttpStatus.CREATED);
     }
 
 }
